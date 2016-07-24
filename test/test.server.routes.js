0a816fe93a5a50c8df23b72cf0b8da36a3e62a09
Tjen Wellens <(none)>
2016-07-24 15:11:17 +0200

    fix tests

--- test/test.server.routes.js
+++ test/test.server.routes.js
@@ -5,11 +5,17 @@ var config = require('../server/config');
 var app = require('../server/app')(config);
 
 describe('server routes', function () {
-  it('/ should return html', function (next) {
+  it('/ should exits', function (next) {
     supertest(app)
       .get('/')
       .expect(200)
-      .expect('<!DOCTYPE html><html lang="en"><head><title>Title</title><script type="text/javascript">if (foo) bar(1 + 5)</script></head><body><h1>Jade - node template engine</h1><div id="container" class="col"><p>You are amazing</p><p>Jade is a terse and simple templating language with a\nstrong focus on performance and powerful features.</p></div></body></html>')
+      .end(next);
+  });
+
+  it('/stylesheets/style.css should exits', function (next) {
+    supertest(app)
+      .get('/stylesheets/style.css')
+      .expect(200)
       .end(next);
   });
 });
