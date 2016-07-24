44ff1fc379cdbbf0c8875f5d9af735b64c27d97b
Tjen Wellens <(none)>
2016-07-24 14:56:12 +0200

    added stylus, logging, static folder

--- server/app.js
+++ server/app.js
@@ -1,16 +1,33 @@
 var express = require('express');
+var morgan = require('morgan');
+var stylus = require('stylus');
+var nib = require('nib');
 
-module.exports = function () {
+module.exports = function (config) {
   var app = express();
 
   app.set('view engine', 'jade');
 
+  app.use(morgan('dev'));
+
+  app.use(stylus.middleware({
+    src: config.path('stylesheets'),
+    dest: config.path('public/stylesheets'),
+    compile: compileStylus
+  }));
+
+  app.use(express.static(config.path('public')));
+
   app.get('/', function (req, res) {
-    res.render('index', {
-      pageTitle: "Title",
-      youAreUsingJade: true,
-    });
+    res.render('index', {title: 'Home'});
   });
 
   return app;
 };
+
+function compileStylus (str, path) {
+  return stylus(str)
+    .set('filename', path)
+    .set('compress', true)
+    .use(nib());
+}
--- server/config.js
+++ server/config.js
@@ -1,6 +1,13 @@
+var path = require('path');
+
+var basedir = path.join(__dirname, '../');
+
 module.exports = {
   app: {
     port: process.env.PORT || 8080,
     name: 'Deliberate Experiment'
+  },
+  path: function (p) {
+    return path.join(basedir, p || '.');
   }
 };
