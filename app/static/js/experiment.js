05126fd633357a6efa89c28e9a251057f1a80127
Tjen Wellens <(none)>
2016-07-31 22:25:33 +0200

    improved code to swap snippet

--- app/static/js/experiment.js
+++ app/static/js/experiment.js
@@ -1,19 +1,39 @@
 /* global $:false */
 $(document).ready(function () {
-  $('#selectLanguage').change(function () {
+  // listen to changes of selected language
+  $('#selectLanguage').change(refreshCodeSnippet());
+
+  // load default language
+  refreshCodeSnippet();
+
+  function refreshCodeSnippet () {
+    var $code = $('#code');
+
+    // show loading message
+    $code.html('<p>Loading, please wait...</p>');
+
+    // get selected language
     var language = $('#selectLanguage').val();
+
+    // create ajax call url
     var url = '/api/code/' + language;
+
+    // do ajax call
     $.get(url, function (jsend) {
-      if (!jsend.success === 'success') {
+      if (jsend.status !== 'success') {
         console.log('Error with ajax call(' + url + '): ');
         console.log(jsend);
         alert('Error with ajax call(' + url + ')');
+        return;
       }
 
-      var lines = jsend.data.code;
-      var code = '<p>' + lines.join('<br>') + '</p>';
-
-      $('#code').html(code);
+      insertSnippet($code, jsend.data);
     });
-  });
+  }
+
+  function insertSnippet (element, snippet) {
+    var lines = snippet.code;
+    var code = '<p>' + lines.join('<br>') + '</p>';
+    element.html(code);
+  }
 });
