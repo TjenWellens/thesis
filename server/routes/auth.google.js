66f0b8ede3d87ea7d6ce9b6b6c1e9bc5e9fe545c
Tjen Wellens <(none)>
2016-08-01 15:10:00 +0200

    refactor auth routes

--- server/routes/auth.google.js
+++ server/routes/auth.google.js
@@ -5,65 +5,67 @@ var Strategy = require('passport-google-oauth').OAuth2Strategy;
 module.exports = function (app, config, model) {
   var User = model.user;
 
-  var strategy = new Strategy(
-    config.auth.google,
-    function (token, tokenSecret, profile, done) {
-      var userData = {
-        provider: 'google',
-        google: {
-          id: profile.id,
-        }
-      };
-
-      User.findOne(userData)
-        .then(successIfUserExists)
-        .then(createNewUser)
-        .then(save)
-        .then(success)
-        .catch(doneIfError);
-
-      //region Helper Functions
-      function save (user) {
-        return user.save();
-      }
-
-      function success (user) {
-        done(null, user);
-      }
-
-      function createNewUser () {
-        return new User({
+  function createStrategy () {
+    return new Strategy(
+      config.auth.google,
+      function (token, tokenSecret, profile, done) {
+        var userData = {
           provider: 'google',
-          name: profile.displayName,
           google: {
             id: profile.id,
-            email: getAccountEmail(profile.emails)
           }
-        });
-      }
+        };
 
-      function getAccountEmail (emails) {
-        var accountEmail = _.find(emails, function (email) {
-          return email.type === 'account';
-        });
-        var firstAccountEmailAddress = accountEmail.value;
-        return firstAccountEmailAddress;
-      }
+        User.findOne(userData)
+          .then(successIfUserExists)
+          .then(createNewUser)
+          .then(save)
+          .then(success)
+          .catch(doneIfError);
 
-      function successIfUserExists (user) {
-        if (user) {
-          done(null, user);
-          return Promise.reject(null);
+        //region Helper Functions
+        function save (user) {
+          return user.save();
         }
-      }
 
-      function doneIfError (err) {
-        if (err) done(err);
-      };
-      //endregion
-    });
+        function success (user) {
+          done(null, user);
+        }
 
-  passport.use(strategy);
+        function createNewUser () {
+          return new User({
+            provider: 'google',
+            name: profile.displayName,
+            google: {
+              id: profile.id,
+              email: getAccountEmail(profile.emails)
+            }
+          });
+        }
+
+        function getAccountEmail (emails) {
+          var accountEmail = _.find(emails, function (email) {
+            return email.type === 'account';
+          });
+          var firstAccountEmailAddress = accountEmail.value;
+          return firstAccountEmailAddress;
+        }
+
+        function successIfUserExists (user) {
+          if (user) {
+            done(null, user);
+            return Promise.reject(null);
+          }
+        }
+
+        function doneIfError (err) {
+          if (err) done(err);
+        };
+        //endregion
+      });
+  }
+
+  passport.use(createStrategy());
 
   app.get('/login/google',
     passport.authenticate('google', {scope: ['email']}));
--- server/routes/auth.local.js
+++ server/routes/auth.local.js
@@ -14,107 +14,111 @@ module.exports = function (app, config, model) {
     },
   }
 
-  var loginStrategy = new LocalStrategy(config.auth.localstrategy,
-    function (req, email, password, done) {
-      if (!email)return done(null, false, req.flash('login', 'Email required'));
-      if (!password) return done(null, false, req.flash('login', 'Password required'));
+  function createLoginStrategy () {
+    return new LocalStrategy(config.auth.localstrategy,
+      function (req, email, password, done) {
+        if (!email)return done(null, false, req.flash('login', 'Email required'));
+        if (!password) return done(null, false, req.flash('login', 'Password required'));
 
-      var searchData = {
-        provider: 'local',
-        local: {
-          email: email,
-        }
-      };
-
-      User.findOne(searchData)
-        .then(failIfUserDoesNotExists)
-        .then(checkPassword)
-        .then(success)
-        .catch(doneIfError);
-
-      //region Helper Functions
-      function failIfUserDoesNotExists (user) {
-        if (!user) {
-          console.log('login failed: email does not exist')
-          done(null, false, req.flash('signup', 'That email does not exist'));
-          return Promise.reject(null);
-        }
-        return user;
-      }
-
-      function checkPassword (user) {
-        if (!util.comparePassword(password, user.local.password)) {
-          done(null, false, req.flash('login', 'Incorrect password.'));
-          return Promise.reject(null);
-        }
-        return user;
-      }
-
-      function success (user) {
-        done(null, user);
-      }
-
-      function doneIfError (err) {
-        if (err) done(err);
-      };
-      //endregion
-    });
-
-  var signupStrategy = new LocalStrategy(config.auth.localstrategy,
-    function (req, email, password, done) {
-      if (!email)return done(null, false, req.flash('signup', 'Email required'));
-      if (!password) return done(null, false, req.flash('signup', 'Password required'));
-
-      var searchData = {
-        provider: 'local',
-        local: {
-          email: email,
-        }
-      };
-
-      User.findOne(searchData)
-        .then(failIfUserExists)
-        .then(createNewUser)
-        .then(save)
-        .then(success)
-        .catch(doneIfError);
-
-      //region Helper Functions
-      function save (user) {
-        return user.save();
-      }
-
-      function success (user) {
-        done(null, user);
-      }
-
-      function createNewUser () {
-        return new User({
+        var searchData = {
           provider: 'local',
-          name: req.body.name,
           local: {
             email: email,
-            password: util.hashPassword(password),
-            registeredOn: new Date(),
           }
-        });
-      }
+        };
 
-      function failIfUserExists (user) {
-        if (user) {
-          done(null, false, req.flash('signup', 'That email is already taken.'));
-          return Promise.reject(null);
+        User.findOne(searchData)
+          .then(failIfUserDoesNotExists)
+          .then(checkPassword)
+          .then(success)
+          .catch(doneIfError);
+
+        //region Helper Functions
+        function failIfUserDoesNotExists (user) {
+          if (!user) {
+            console.log('login failed: email does not exist')
+            done(null, false, req.flash('signup', 'That email does not exist'));
+            return Promise.reject(null);
+          }
+          return user;
         }
-      }
 
-      function doneIfError (err) {
-        if (err) done(err);
-      };
-      //endregion
-    });
+        function checkPassword (user) {
+          if (!util.comparePassword(password, user.local.password)) {
+            done(null, false, req.flash('login', 'Incorrect password.'));
+            return Promise.reject(null);
+          }
+          return user;
+        }
 
-  passport.use(loginStrategy);
-  passport.use('local-signup', signupStrategy);
+        function success (user) {
+          done(null, user);
+        }
+
+        function doneIfError (err) {
+          if (err) done(err);
+        };
+        //endregion
+      });
+  }
+
+  function createSignupStrategy () {
+    return new LocalStrategy(config.auth.localstrategy,
+      function (req, email, password, done) {
+        if (!email)return done(null, false, req.flash('signup', 'Email required'));
+        if (!password) return done(null, false, req.flash('signup', 'Password required'));
+
+        var searchData = {
+          provider: 'local',
+          local: {
+            email: email,
+          }
+        };
+
+        User.findOne(searchData)
+          .then(failIfUserExists)
+          .then(createNewUser)
+          .then(save)
+          .then(success)
+          .catch(doneIfError);
+
+        //region Helper Functions
+        function save (user) {
+          return user.save();
+        }
+
+        function success (user) {
+          done(null, user);
+        }
+
+        function createNewUser () {
+          return new User({
+            provider: 'local',
+            name: req.body.name,
+            local: {
+              email: email,
+              password: util.hashPassword(password),
+              registeredOn: new Date(),
+            }
+          });
+        }
+
+        function failIfUserExists (user) {
+          if (user) {
+            done(null, false, req.flash('signup', 'That email is already taken.'));
+            return Promise.reject(null);
+          }
+        }
+
+        function doneIfError (err) {
+          if (err) done(err);
+        };
+        //endregion
+      });
+  }
+
+  passport.use(createLoginStrategy());
+  passport.use('local-signup', createSignupStrategy());
 
   app.post('/signup', passport.authenticate('local-signup', {
       successRedirect: '/',
