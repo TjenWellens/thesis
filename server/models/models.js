9cbc407a6ac8cd967be4461c20dc6d4a870ed373
Tjen Wellens <(none)>
2016-08-02 20:54:18 +0200

    inject mongoose in server.js

--- server/models/models.js
+++ server/models/models.js
@@ -1,13 +1,8 @@
-var mongoose = require('mongoose');
-
-var User = require('./user')(mongoose);
-var Code = require('./code')(mongoose);
-var Experiment = require('./experiment')(mongoose);
-var Contact = require('./contact')(mongoose);
-
-module.exports = {
-  user: User,
-  code: Code,
-  experiment: Experiment,
-  contact: Contact,
+module.exports = function (mongoose) {
+  return {
+    user: require('./user')(mongoose),
+    code: require('./code')(mongoose),
+    experiment: require('./experiment')(mongoose),
+    contact: require('./contact')(mongoose),
+  };
 };
--- test/models/models.js
+++ test/models/models.js
@@ -2,16 +2,8 @@ var mongoose = require('../../util/fake-mongoose');
 var path = require('path');
 var seed = require('../../util/seed-database');
 
-var User = require('../../server/models/user')(mongoose);
-var Code = require('../../server/models/code')(mongoose);
-var Experiment = require('../../server/models/experiment')(mongoose);
-var Contact = require('../../server/models/contact')(mongoose);
+var models = require('../../server/models/models')(mongoose);
 
-seed(Code, path.join(__dirname, 'languages.json'));
+seed(models.code, path.join(__dirname, 'languages.json'));
 
-module.exports = {
-  user: User,
-  code: Code,
-  experiment: Experiment,
-  contact: Contact,
-};
+module.exports = models;
