d6dfe9b63c8e8d6c5f34f4a46b349858340eaa24
Tjen Wellens <(none)>
2016-08-03 15:36:30 +0200

    seed.js

--- languages/languages.js
+++ languages/languages.js
@@ -5,7 +5,7 @@ var lineReader = require('readline');
 
 function fromJsonFile () {
   var languageSettings = JSON.parse(readFile(path.join(__dirname, 'languages.json')));
-  _.map(languageSettings, function (languageSetting) {
+  return _.map(languageSettings, function (languageSetting) {
     var title = languageSetting.title;
     var name = languageSetting.name;
     var file = path.join(__dirname, name + '.txt');
---
+++ languages/seed.js
@@ -0,0 +1,26 @@
+var _ = require('underscore');
+var mongoose = require('mongoose');
+var models = require('../server/models/models')(mongoose);
+
+require('dotenv').config();
+var config = require('../server/config');
+var getSnippets = require('./languages');
+
+// connect to MongoDB
+mongoose.Promise = global.Promise;
+mongoose.connect(config.database.url)
+  .then(function () {
+    console.log('---Connected to db ' + config.database.url);
+  })
+  .then(function () {
+    var Code = models.code;
+    var snippets = getSnippets();
+    _.each(snippets, function (snippet) {
+      new Code(snippet).save();
+    });
+  })
+  .catch(function (err) {
+    console.error('Failed to connect with database: ', err);
+    console.log('Failed to connect with database: ', err);
+    process.exit(1);
+  });
