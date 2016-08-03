e773b5c0d38081ffe0fac9c00cce95d2d2e784b2
Tjen Wellens <(none)>
2016-08-03 15:26:24 +0200

    working

--- languages/languages.js
+++ languages/languages.js
@@ -3,13 +3,13 @@ var path = require('path');
 var readFile = require('../util/read-file');
 var lineReader = require('readline');
 
-function fromJsonFile() {
+function fromJsonFile () {
   var languageSettings = JSON.parse(readFile(path.join(__dirname, 'languages.json')));
   _.map(languageSettings, function (languageSetting) {
     var title = languageSetting.title;
     var name = languageSetting.name;
     var file = path.join(__dirname, name + '.txt');
-    var code = readCodeLines(file);
+    var code = readFile(file).split('\n');
 
     return {
       language: name,
@@ -19,12 +19,4 @@ function fromJsonFile() {
   });
 }
 
-function readCodeLines (file) {
-  var all = readFile(file);
-  console.log(all);
-  return [];
-}
-
 module.exports = fromJsonFile
-
-fromJsonFile();
