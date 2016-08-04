0822f9bf9ef0b57f90d8db8b14e880de753952bf
Tjen Wellens <(none)>
2016-08-04 17:44:15 +0200

    miner.js 4 spaces

--- seed/snippets/miner.js
+++ seed/snippets/miner.js
@@ -1,34 +1,34 @@
 function solve (map, start, exit) {
-  if (!map[start.x][start.y]) {
-    return null;
-  }
-  if (start.x === exit.x && start.y === exit.y) {
-    return [];
-  }
+    if (!map[start.x][start.y]) {
+        return null;
+    }
+    if (start.x === exit.x && start.y === exit.y) {
+        return [];
+    }
 
-  var beginNode = new Node(start.x, start.y, null);
-  beginNode.getAdjacentNodes();
+    var beginNode = new Node(start.x, start.y, null);
+    beginNode.getAdjacentNodes();
 
-  var frontier = beginNode.childrenNodes;
-  var path = null;
+    var frontier = beginNode.childrenNodes;
+    var path = null;
 
-  for (var x = 0; x < (map.length * map[0].length), path === null; x++) {
-    frontier.forEach(function (node, index) {
-      frontier.splice(index, 1);
+    for (var x = 0; x < (map.length * map[0].length), path === null; x++) {
+        frontier.forEach(function (node, index) {
+            frontier.splice(index, 1);
 
-      if (node.x === exit.x && node.y === exit.y) {
-        path = node.getPath();
-      }
+            if (node.x === exit.x && node.y === exit.y) {
+                path = node.getPath();
+            }
 
-      node.getAdjacentNodes();
+            node.getAdjacentNodes();
 
-      if (node.childrenNodes.length > 0) {
-        node.childrenNodes.forEach(function (n) {
-          frontier.push(n);
+            if (node.childrenNodes.length > 0) {
+                node.childrenNodes.forEach(function (n) {
+                    frontier.push(n);
+                });
+            }
         });
-      }
-    });
-  }
+    }
 
-  return path;
+    return path;
 }
