3c438fd8ecfee930073a6e30b6016344dfc3176b
Tjen Wellens <(none)>
2016-08-20 13:38:03 +0200

    removed whitespace, added sanity check

--- seed/snippets/miner.js
+++ seed/snippets/miner.js
@@ -1,20 +1,21 @@
 function solve (map, start, exit) {
+    if (!map.contains(start) || !map.contains(exit)) {
+      return null;
+    }
+    if (start.equals(exit)) {
+        return [];
+    }
     var frontier = start.getNeighbors();
-
     for (var i = 0; i < map.rows * map.cols; i++) {
         for (var frontierIndex = 0; frontierIndex < frontier.length; frontierIndex++) {
             var tile = frontier[frontierIndex];
-
             frontier.remove(tile);
-
             if (tile.equals(exit)) {
                 return tile.getPath();
             }
-
             var neighbors = tile.getNeighbors();
             frontier = frontier.concat(neighbors);
         }
     }
-
     return null;
 }
